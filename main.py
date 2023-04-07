c82479c4934bf90611a45651aa66db351e4d0c46
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-04-07 16:23:14 +0800

    modify

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         topFrame = ttk.LabelFrame(self,text="台北市行政區")
-        print(datasource.sarea_list)
+        # print(datasource.sarea_list)
         length = len(datasource.sarea_list)
+        self.radioStringVar = tk.StringVar()
         for i in range(length):
             cols = i %3
             rows = i //3
-            ttk.Radiobutton(topFrame,text=datasource.sarea_list[i]).grid(column=cols,row=rows)
+            ttk.Radiobutton(topFrame,text=datasource.sarea_list[i],value=datasource.sarea_list[i],variable=self.radioStringVar,command=self.radio_Event).grid(column=cols,row=rows,sticky=tk.W,padx=10,pady=10)
+
         topFrame.pack()
+        self.radioStringVar.set('信義區')
+
+    def radio_Event(self):
+        print(self.radioStringVar.get())
+        area_name = self.radioStringVar.get()
+        area_data = datasource.getInfoFromArea(area_name)
+        for item in area_data:
+            print(item)
 
 def main():
     window = Window()
