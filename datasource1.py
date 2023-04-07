8191b17d1b5df7ec778b6615e8b191bb4a239722
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-04-07 14:50:41 +0800

    modify

--- datasource1.py
+++ datasource1.py
@@ -7,11 +7,10 @@ def getInfo():
     url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
     response = requests.get(url)
     data_list = response.json()
-    sarea_temp = set ()
+    sarea_temp = set()
     for item in data_list:
         sarea_temp.add(item["sarea"])
-        print (item["sarea"])
     sarea_list = list(sarea_temp)
+    
 
-
-    getInfo()   
+getInfo()
