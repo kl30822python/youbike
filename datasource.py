84611b1fb3bb9797b4d69a734d7c4e9a26f30dac
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-04-12 15:27:47 +0800

    modify

--- datasource.py
+++ datasource.py
@@ -1,11 +1,12 @@
 import requests
 
+# Store the area names and data
 sarea_list = None
 data_list = None
 
 def getInfo() -> None:
-        # Get data from TCGB website
     global sarea_list, data_list
+    # Get data from TCGB website
     url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
     response = requests.get(url)
     data_list = response.json()
@@ -16,15 +17,15 @@ def getInfo() -> None:
     sarea_list = sorted(list(sarea_temp))
 
 def getInfoFromArea(areaName) -> list:
-    filter_data = filter(lambda n:n["sarea"]==areaName, data_list)
+    filter_data = filter(lambda n:n["sarea"]==areaName,data_list)
     return list(filter_data)
-
+    
 def filter_sbi_warning_data(area_data,numbers) -> list:
     filter_data = filter(lambda n:n['sbi']<=numbers,area_data)
     return list(filter_data)
 
-def filter_bemp_warning_data(area_data,numbers) -> list:
-    filter_data = filter(lambda n:n['bemp']<=numbers,area_data)
+def filter_bemp_warning_data(area_data, numbers) -> list:
+    filter_data = filter(lambda n: n['bemp'] <= numbers, area_data)
     return list(filter_data)
 
 getInfo()
