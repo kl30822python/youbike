a2114e59b1853496c808353738e055d785aa886b
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-04-13 13:45:06 +0800

    modify buttonbox

--- messageWindow/display.py
+++ messageWindow/display.py
@@ -9,9 +9,16 @@ class MapDisplay(Dialog):
     
     #override
     def body(self,master):
-        closeButton = ttk.Button(self,text="close")
-        closeButton.pack(padx=30,pady=30)
+        pass
+
     #override
     
     def buttonbox(self):
-        pass
+        '''add standard button box.
+        override if you do not want the standard buttons
+        '''
+        boxFrame = ttk.Frame(self)
+        button = ttk.Button(boxFrame, text="關閉", width=10, command=self.ok, default=tk.ACTIVE) #self.ok方法是繼承來的
+        button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.bind("<Return>", self.ok)
+        boxFrame.pack()
